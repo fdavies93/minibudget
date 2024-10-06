5576e086b6ff458e97722b21528fc46ee51cf06b
Frank Davies <fdavies93@gmail.com>
2024-10-06 22:41:22 +0800

    Get chart of accounts working and include summary headers in report

--- minibudget.py
+++ minibudget.py
@@ -1,6 +1,8 @@
 from argparse import ArgumentParser
 from dataclasses import dataclass
 import sys
+from copy import deepcopy
+from typing import Union
 
 @dataclass
 class Entry:
@@ -8,6 +10,11 @@ class Entry:
     is_expense: bool
     amount: int
     currency: str
+
+@dataclass
+class EntryTreeNode:
+    entry: Union[Entry,None]
+    children: dict[str, "EntryTreeNode"]
 
 def tokenise_line(line: str) -> list[str]:
     delimiter = "\""
@@ -71,47 +78,69 @@ def calculate_totals(entries: list[Entry]):
 
     return totals
 
-def generate_income_report(entries: list[Entry]):
-    lines = []
-
+def generate_category_tree(entries: list[Entry]) -> EntryTreeNode:
+    root = EntryTreeNode(None, {}) 
     for entry in entries:
-        category_tag = "/".join(entry.categories)
-        amount_part = f"{entry.amount} {entry.currency}"
-        spacer = (80-(len(category_tag) + len(amount_part)))*" "
-        if not entry.is_expense:
-            lines.append(f"{category_tag}{spacer}{amount_part}")
-    return "\n".join(lines)
+        cur = root
+        categories = deepcopy(entry.categories)
+        while len(categories) > 0:
+            next = categories[0]
+            categories.remove(next)
+            if next not in cur.children:
+                cur.children[next] = EntryTreeNode(None, {}) 
+            cur = cur.children[next]
+        if cur.entry is not None:
+            raise ValueError("Cannot have two categories with identical names!")
+        cur.entry = entry
+    return root
 
-def generate_expense_report(entries: list[Entry]):
+def format_category_tree(tree: EntryTreeNode, width=80, depth=0) -> list[str]:
     lines = []
-    for entry in entries:
-        category_tag = "/".join(entry.categories)
-        amount_part = f"{entry.amount} {entry.currency}"
-        spacer = (80-(len(category_tag) + len(amount_part)))*" "
-        if entry.is_expense:
-            lines.append(f"{category_tag}{spacer}{amount_part}")
-    return "\n".join(lines)
+    for tag, child in tree.children.items():
+        left = f"{"    "*depth}{tag}"
+        right = ""
+        if child.entry is not None:
+            right = f"{child.entry.amount} {child.entry.currency}"
+        spacer = " " * (width - (len(left) + len(right)))
+        lines.append(f"{left}{spacer}{right}")
+        lines.extend(format_category_tree(child, 80, depth+1))
+    return lines
 
+def generate_total_header(heading: str, total: int, currency: str, width=80) -> list[str]:
+    lines = ["-" * width] 
+   
+    right = f"{total} {currency}"
+    spacer = " " * ( width - (len(heading) + len(right)) )
+    lines.append(f"{heading}{spacer}{right}")
+    lines.append("-" * width)
+    return lines
+
+def generate_income_report(entries: list[Entry], width=80):
+    income_entries = list(filter(lambda e: not e.is_expense, entries))
+    total = sum([e.amount for e in income_entries])
+    tree = generate_category_tree(income_entries)
+    output = generate_total_header("INCOME", total, income_entries[0].currency, width)
+    output.extend(format_category_tree(tree, width))
+    return "\n".join(output)
+    
+def generate_expense_report(entries: list[Entry], width=80):
+    expense_entries = list(filter(lambda e: e.is_expense, entries))
+    total = sum([e.amount for e in expense_entries])
+    tree = generate_category_tree(expense_entries)
+    output = generate_total_header("EXPENSES", total, expense_entries[0].currency, width)
+    output.extend(format_category_tree(tree, width))
+    return "\n".join(output)
 
 def report_budget(entries: list[Entry]):
     totals = calculate_totals(entries)
-    
-    separator = "-"*80
-
-    print(separator)
-    print("INCOME")
-    print(separator)
+     
     print(generate_income_report(entries))
-    print(separator)
-    print("EXPENSES")
-    print(separator)
     print(generate_expense_report(entries))
-    print(separator)
-    print("UNASSIGNED")
-    print(separator)
-    for key, value in totals.items():
-        print(f"{value} {key}")
-    print(separator)
+
+    total_head = entries[0].currency
+    print(
+        "\n".join(generate_total_header("UNASSIGNED",totals[total_head],total_head, 80))
+    )
 
 def main():
     parser = ArgumentParser()
